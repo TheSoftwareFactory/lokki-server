e54c3f48c3f2291485a344b4dc8bf5e624bb62b8
Grandi <kari.rantanen@helsinki.fi>
2015-09-10 13:00:39 +0300

    Fixed more lint errors

--- locmap/lib/crashReports.js
+++ locmap/lib/crashReports.js
@@ -17,17 +17,18 @@ var CrashReports = function() {
 
     // userId is used to differentiate reports, as timestamp alone can conflict.
     this.store = function(userId, osType, reportObject, callback) {
-        var date = new Date();
+        var date = new Date(),
         // We can get all reports per month with the same hgetall redis call.
-        var key = crashReportPrefix + osType + ':' + date.getFullYear() + ':' + (date.getMonth() + 1);
-        // Field differentiates within the key, using unix time in milliseconds, should be good enough for now.
-        var field = userId + ':' + date.getTime();
-
-        var data = {
-            timestamp: date.getTime(),
-            osType: osType,
-            report: reportObject
-        };
+            key = crashReportPrefix + osType + ':' +
+                date.getFullYear() + ':' + (date.getMonth() + 1),
+        // Field differentiates within the key, using
+        // unix time in milliseconds, should be good enough for now.
+            field = userId + ':' + date.getTime(),
+            data = {
+                timestamp: date.getTime(),
+                osType: osType,
+                report: reportObject
+            };
         db.hset(key, field, JSON.stringify(data), function(error) {
             if (error) {
                 callback(400, 'Database write failed');
@@ -38,17 +39,18 @@ var CrashReports = function() {
     };
 
     this.getMonth = function(osType, year, month, callback) {
-        var redisKey = crashReportPrefix + osType + ':' + year + ':' + month;
-        var reports = {};
+        var redisKey = crashReportPrefix + osType + ':' + year + ':' + month,
+            reports = {};
         db.hgetall(redisKey, function(error, result) {
+            var key, item, date, isoStamp = null;
             if (result) {
-                for (var key in result) {
+                for (key in result) {
                     if (result.hasOwnProperty(key)) {
                         if (result[key] !== null && result[key] !== undefined) {
-                            var item = JSON.parse(result[key]);
+                            item = JSON.parse(result[key]);
                             // Attempt to convert timestamp to human readable format.
-                            var date = new Date(item.timestamp);
-                            var isoStamp = date.toISOString();
+                            date = new Date(item.timestamp);
+                            isoStamp = date.toISOString();
                             item.timestamp = isoStamp;
                             reports[isoStamp + ' ' + key] = item;
                         }
