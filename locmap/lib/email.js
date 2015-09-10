7535a86ce9fd89341779f64e4369a4242f1a478c
Grandi <kari.rantanen@helsinki.fi>
2015-09-10 13:23:26 +0300

    Lint errors fixed in email.js

--- locmap/lib/email.js
+++ locmap/lib/email.js
@@ -17,18 +17,18 @@ var locMapCommon = new LocMapCommon();
 var I18N = require('../../lib/i18n');
 var i18n = new I18N();
 
-var LocMapEmail = function() {
+var LocMapEmail = function () {
     this.emails = [];
 
-    this._sendEmail = function(emailObj, callback) {
+    this._sendEmail = function (emailObj, callback) {
         if (conf.get('sendEmails')) {
             logger.trace('Using sendgrid to send email.');
             var sendgrid = require('sendgrid')(
-                conf.get('sendGrid').username,
-                conf.get('sendGrid').password
-            );
-            var email = new sendgrid.Email(emailObj);
-            sendgrid.send(email, function(err) {
+                    conf.get('sendGrid').username,
+                    conf.get('sendGrid').password
+                ),
+                email = new sendgrid.Email(emailObj);
+            sendgrid.send(email, function (err) {
                 if (err) {
                     logger.error('Error sending signup email to ' + emailObj.to + ' : ' + err);
                     logger.error(err);
@@ -44,28 +44,28 @@ var LocMapEmail = function() {
         }
     };
 
-    this.sendSignupMail = function(targetEmail, langCode, callback) {
-        var lang = locMapCommon.verifyLangCode(langCode);
-        var subject = i18n.getLocalizedString(lang, 'signup.userEmailSubject');
-        var messageText = i18n.getLocalizedString(lang, 'signup.userEmailText');
-
-        var emailObj = {
-            to: targetEmail,
-            from: conf.get('senderEmail'),
-            subject: subject,
-            text: messageText
-        };
+    this.sendSignupMail = function (targetEmail, langCode, callback) {
+        var lang = locMapCommon.verifyLangCode(langCode),
+            subject = i18n.getLocalizedString(lang, 'signup.userEmailSubject'),
+            messageText = i18n.getLocalizedString(lang, 'signup.userEmailText'),
+            emailObj = {
+                to: targetEmail,
+                from: conf.get('senderEmail'),
+                subject: subject,
+                text: messageText
+            };
         this._sendEmail(emailObj, callback);
     };
 
-    this.sendInviteEmail = function(targetEmail, inviterEmail, langCode, callback) {
+    this.sendInviteEmail = function (targetEmail, inviterEmail, langCode, callback) {
         if (conf.get('sendInviteEmails')) {
-            logger.trace('SendInviteEmail with ' + targetEmail + ' ' + inviterEmail + ' ' + langCode);
-            var lang = locMapCommon.verifyLangCode(langCode);
-            var subject = i18n.getLocalizedString(lang, 'invite.userInvitedToLokkiEmailSubject');
-            var messageText = i18n.getLocalizedString(lang, 'invite.userInvitedToLokkiEmailText', 'targetUser', targetEmail, 'senderUser', inviterEmail);
-
-                var emailObj = {
+            logger.trace('SendInviteEmail with ' + targetEmail +
+                ' ' + inviterEmail + ' ' + langCode);
+            var lang = locMapCommon.verifyLangCode(langCode),
+                subject = i18n.getLocalizedString(lang, 'invite.userInvitedToLokkiEmailSubject'),
+                messageText = i18n.getLocalizedString(lang, 'invite.userInvitedToLokkiEmailText',
+                    'targetUser', targetEmail, 'senderUser', inviterEmail),
+                emailObj = {
                     to: targetEmail,
                     from: conf.get('senderEmail'),
                     subject: subject,
@@ -73,28 +73,29 @@ var LocMapEmail = function() {
                 };
             this._sendEmail(emailObj, callback);
         } else {
-            logger.trace('SendInviteEmail disabled. Target: ' + targetEmail + ' Inviter: ' + inviterEmail + ' Lang: ' + langCode);
+            logger.trace('SendInviteEmail disabled. Target: ' + targetEmail +
+                ' Inviter: ' + inviterEmail + ' Lang: ' + langCode);
             callback(true);
         }
     };
 
     /*
-    this._generateResetText = function(targetEmail, resetLink) {
+    this._generateResetText = function (targetEmail, resetLink) {
         return "Dear " + targetEmail + ",\n\n" + LocMapCommon.messageTexts.resetEmail1 + resetLink +
             LocMapCommon.messageTexts.resetEmail2;
     };
     */
 
-    this.sendResetEmail = function(targetEmail, resetLink, langCode, callback) {
-        var lang = locMapCommon.verifyLangCode(langCode);
-        var subject = i18n.getLocalizedString(lang, 'reset.emailSubject');
-        var messageText = i18n.getLocalizedString(lang, 'reset.emailText', 'resetLink', resetLink);
-        var emailObj = {
-            to: targetEmail,
-            from: conf.get('senderEmail'),
-            subject: subject,
-            text: messageText
-        };
+    this.sendResetEmail = function (targetEmail, resetLink, langCode, callback) {
+        var lang = locMapCommon.verifyLangCode(langCode),
+            subject = i18n.getLocalizedString(lang, 'reset.emailSubject'),
+            messageText = i18n.getLocalizedString(lang, 'reset.emailText', 'resetLink', resetLink),
+            emailObj = {
+                to: targetEmail,
+                from: conf.get('senderEmail'),
+                subject: subject,
+                text: messageText
+            };
         this._sendEmail(emailObj, callback);
     };
 };
