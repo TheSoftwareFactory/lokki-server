28ba4b7dc565e9fc4876d787e9d95c5684bb73ac
suleymanakbas91 <suleymanakbas91@gmail.com>
2015-11-26 16:43:41 +0200

    Checking the version and notifying if it is less than least accepted version.

---
+++ locmap/lib/constants.js
@@ -0,0 +1,15 @@
+/**
+ * Created by suakbas on 25.11.2015.
+ */
+'use strict';
+
+/*
+ * Constants
+ */
+
+var Constants = {
+    "LatestAcceptedVersionCode":"413",
+    "ServerMessage":"Your application is out of date. Please download the new version from Google Play Store."
+};
+
+module.exports = Constants;
